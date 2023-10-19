042f7f35b98243bf15151ca90eff7e1d694e4986
VITOR SANTANA DE JESUS <vitor.alunos@cfp127aluno.local>
2023-10-19 15:57:52 -0300

    first

---
+++ src/br/senai/sp/jandira/GerenciadorFormas.java
@@ -0,0 +1,9 @@
+package br.senai.sp.jandira;
+
+public class GerenciadorFormas {
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ src/br/senai/sp/jandira/model/FormaGeometrica.java
@@ -0,0 +1,15 @@
+package br.senai.sp.jandira.model;
+
+public class FormaGeometrica {
+
+
+    public void calcularArea(){
+
+    }
+
+    public void calcularPerimetro(){
+
+    }
+
+
+}
